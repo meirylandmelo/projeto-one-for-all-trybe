58b4db3a4a461d273d87c79dcdc9ac11ebca1fe0
Meiryland Melo <meirylandmelo@gmail.com>
2022-03-10 12:09:25 -0300

    adiciona requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,7 @@
+SELECT 
+	art.name AS artista,
+    alb.name AS album
+FROM SpotifyClone.artist AS art
+INNER JOIN SpotifyClone.album AS alb ON art.artist_id = alb.artist_id
+WHERE art.name = 'Walter Phoenix'
+ORDER BY album;
